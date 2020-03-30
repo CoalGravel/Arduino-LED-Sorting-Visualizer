a5dbfdc44cf08a1d381eaaaf8ed04546b7ee2cf2
Evan Chu <echu875615@gmail.com>
2020-03-30 16:45:51 -0400

    Added leds_state array which keeps track of the LED's rgb value. Initializes the array to be a gradient of reds for now.

--- test/test.ino
+++ test/test.ino
@@ -5,11 +5,26 @@ struct RGB {
   byte v;
 };
 
+#define LED_SIZE 150
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  RGB temp = {1,2,3,1};
-  Serial.println(temp.b);
+  
+  RGB leds_state[LED_SIZE];
+  float curr_val = 0;
+  for(int i = 0; i < LED_SIZE; i++) {
+    RGB temp = {(byte)(curr_val + 0.5), 0, 0, i};
+    leds_state[i] = temp;
+    curr_val = curr_val + (256.0 / LED_SIZE);
+  }
+
+  
+//  for(int j = 0; j < LED_SIZE; j++) {
+//    Serial.println(leds_state[j].r);
+//  } 
+
+  
 }
 
 void loop() {
