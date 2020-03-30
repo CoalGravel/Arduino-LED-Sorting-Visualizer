045f70a696cb629a8a0216a8ef6254bd50b014ea
CoalGravel <cole.gravelle@gmail.com>
2020-03-30 17:20:19 -0400

    fixed syntax/structure

--- visualizer/visualizer.ino
+++ visualizer/visualizer.ino
@@ -11,7 +11,7 @@ void setup() {
   RGB leds_state[LED_SIZE];
   
   float curr_val = 0;
-  for(int i = 0; i < LED_SIZE; i++) {
+  for(size_t i = 0; i < LED_SIZE; i++) {
     RGB temp = {(byte)(curr_val + 0.5), 0, 0, 149 - i};
     leds_state[i] = temp;
     curr_val = curr_val + (256.0 / LED_SIZE);
@@ -27,37 +27,30 @@ void loop() {
 
 }
 
-// A utility function to swap to integers 
-void swap (RGB &a, RGB &b) 
-{ 
+void swap (RGB &a, RGB &b) { 
   RGB temp = a; 
   a = b; 
   b = temp; 
 } 
 
-// A utility function to print an array 
-void print_array (RGB arr[], size_t n) 
-{ 
-  for (size_t i = 0; i < n; i++){
+void print_array (RGB arr[], size_t capacity) { 
+  for (size_t i = 0; i < capacity; i++){
     Serial.print(arr[i].v);
     Serial.print(" ");
   }
   Serial.println();
 } 
 
-// A function to generate a random 
-// permutation of arr[] 
-void randomize (RGB arr[], size_t n) { 
-  for (size_t i = n - 1; i > 0; i--) { 
+void randomize (RGB arr[], size_t capacity) { 
+  for (size_t i = capacity - 1; i > 0; i--) { 
     size_t j = random(i);
     swap(arr[i], arr[j]); 
   } 
 } 
 
-
-void bubble_sort(RGB arr[], int capacity) {
-  for(int i = 0; i < (capacity - 1); i++) {
-    for(int j = 0; j < (capacity - (i + 1)); j ++) {
+void bubble_sort(RGB arr[], size_t capacity) {
+  for(size_t i = 0; i < (capacity - 1); i++) {
+    for(size_t j = 0; j < (capacity - (i + 1)); j ++) {
       if(arr[j].v > arr[j+1].v) {
         swap(arr[j], arr[j+1]);
       }
