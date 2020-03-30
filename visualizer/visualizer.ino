8a5f71a6a812b3c5e74e773bf46db5021b8e4583
CoalGravel <cole.gravelle@gmail.com>
2020-03-30 17:12:39 -0400

    fixed file error

--- visualizer/visualizer.ino
+++ visualizer/visualizer.ino
@@ -1,9 +1,43 @@
-void setup() {
-  // put your setup code here, to run once:
+struct RGB {
+  byte r;
+  byte g;
+  byte b;
+  byte v;
+};
 
+void setup() {
+  Serial.begin(9600);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+// A utility function to swap to integers 
+void swap (RGB &a, RGB &b) 
+{ 
+  RGB temp = a; 
+  a = b; 
+  b = temp; 
+} 
+
+// A utility function to print an array 
+void print_array (RGB arr[], size_t n) 
+{ 
+  for (size_t i = 0; i < n; i++){
+    Serial.print(arr[i].v);
+    Serial.print(" ");
+  }
+  Serial.println();
+} 
+
+// A function to generate a random 
+// permutation of arr[] 
+void randomize (RGB arr[], size_t n) { 
+  for (size_t i = n - 1; i > 0; i--) { 
+    size_t j = random(i);
+    swap(arr[i], arr[j]); 
+  } 
+} 
