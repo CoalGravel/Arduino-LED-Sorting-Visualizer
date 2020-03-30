f68ef8aa259ccf31965ed413c3cc0477982d768c
Evan Chu <echu875615@gmail.com>
2020-03-30 17:15:29 -0400

    pushed changes to visualizer.ino

--- visualizer/visualizer.ino
+++ visualizer/visualizer.ino
@@ -4,10 +4,22 @@ struct RGB {
   byte b;
   byte v;
 };
+#define LED_SIZE 150
 
 void setup() {
   Serial.begin(9600);
+  RGB leds_state[LED_SIZE];
   
+  float curr_val = 0;
+  for(int i = 0; i < LED_SIZE; i++) {
+    RGB temp = {(byte)(curr_val + 0.5), 0, 0, 149 - i};
+    leds_state[i] = temp;
+    curr_val = curr_val + (256.0 / LED_SIZE);
+  }
+
+  print_array(leds_state, LED_SIZE);
+  bubble_sort(leds_state, LED_SIZE);
+  print_array(leds_state, LED_SIZE);
 }
 
 void loop() {
@@ -41,3 +53,14 @@ void randomize (RGB arr[], size_t n) {
     swap(arr[i], arr[j]); 
   } 
 } 
+
+
+void bubble_sort(RGB arr[], int capacity) {
+  for(int i = 0; i < (capacity - 1); i++) {
+    for(int j = 0; j < (capacity - (i + 1)); j ++) {
+      if(arr[j].v > arr[j+1].v) {
+        swap(arr[j], arr[j+1]);
+      }
+    }
+  }
+}
